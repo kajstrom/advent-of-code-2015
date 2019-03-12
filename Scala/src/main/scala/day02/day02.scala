6f722ecac9a9e0bcc60487a71a76920e1618d5da
Kaj Ström <kaj.strom@kapsi.fi>
2019-03-12 20:02:08 +0200

    Day 02, part 1. Scala.

---
+++ Scala/src/main/scala/day02/day02.scala
@@ -0,0 +1,22 @@
+package day02
+
+import scala.io.Source
+
+object day02 extends App{
+  def surfaceArea = (dimensions: Array[Int]) => {
+    val height = dimensions(0)
+    val width = dimensions(1)
+    val length = dimensions(2)
+    val smallestSide = dimensions.min
+
+    val side1 = height * width
+    val side2 = width * length
+    val side3 = length * height
+
+    (2 * side1) + (2 * side2) + (2 * side3) + List(side1, side2, side3).min
+  }
+
+  val input = Source.fromResource("day02.txt").getLines.toList.map(_.split("x").map(_.toInt))
+
+  println("Part 1: ", input.map(dimensions => surfaceArea(dimensions)).sum)
+}
