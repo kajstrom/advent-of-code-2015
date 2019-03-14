f27927f3ec18ab86a16804e6c430958f42a55388
Kaj Ström <kaj.strom@kapsi.fi>
2019-03-14 20:22:49 +0200

    Day 02, part 2. Scala

--- Scala/src/main/scala/day02/day02.scala
+++ Scala/src/main/scala/day02/day02.scala
@@ -19,4 +19,18 @@ object day02 extends App{
   val input = Source.fromResource("day02.txt").getLines.toList.map(_.split("x").map(_.toInt))
 
   println("Part 1: ", input.map(dimensions => surfaceArea(dimensions)).sum)
+
+  def ribbonLengthFor = (dimensions: Array[Int]) => {
+    val sortedDimensions = dimensions.sorted
+    val length = sortedDimensions(0)
+    val width = sortedDimensions(1)
+    val height = sortedDimensions(2)
+
+    val ribbonToWrap = length + length + width + width
+    val ribbonForBow = sortedDimensions.product
+
+    ribbonToWrap + ribbonForBow
+  }
+
+  println("Part 2: ", input.map(dimensions => ribbonLengthFor(dimensions)).sum)
 }
